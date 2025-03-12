7de4346fb86448a731a848405f4c073987b4550d
ejbm10 <eduardo.bm10@estudiantec.cr>
2025-03-11 20:34:44 -0600

    contador terminado

--- Problema_3/ContadorRegresivo.sv
+++ Problema_3/ContadorRegresivo.sv
@@ -1,22 +1,29 @@
 module ContadorRegresivo #(parameter N = 8) (
-	input logic [N - 1:0] init_number,
-	output logic [N - 1:0] current_number
+    input logic clk,
+    input logic rst_async,
+    input logic [N-1:0] init_number,
+    output logic [N-1:0] count
 );
-
-logic [N-1:0] borrows;
-
-generate
-	genvar i;
-	for (i = 0; i < N; i++) begin : gen_block
-		Restador resta1(
-			.a(init_number[i]),
-			.b((i == 0) ? 1'b1 : 1'b0),
-			.cin((i == 0) ? 1'b0 : borrows[i - 1]),
-			.d(current_number[i]),
-			.bo(borrows[i])
-		);
+	logic [N-1:0] next_number;
+	logic [N-1:0] bouts;
+	
+	generate
+		genvar i;
+		for (i = 0; i < N; i++) begin : substractor
+			Restador r1 (
+				.a(count[i]), 
+				.b((i == 0) ? 1'b1 : 1'b0),
+				.cin((i == 0) ? 1'b0 : bouts[i-1]),
+				.d(next_number[i]),
+				.bo(bouts[i])
+			);
+		end
+	endgenerate
+	
+	always_ff @(posedge clk) begin
+		if (!rst_async)
+			count <= init_number;
+		else if (count > 0)
+			count <= next_number;
 	end
-endgenerate
-
-
 endmodule
--- Problema_3/testbench.sv
+++ Problema_3/testbench.sv
@@ -1,18 +1,32 @@
 module testbench();
+
 	parameter N = 8;
-	
-	logic [N-1:0] init_number, current_number;
-	
-	ContadorRegresivo #(.N(N)) c1 (
-		.init_number(init_number),
-		.current_number(current_number)
-	);
-	
-	initial begin
-	
-	init_number = 8'b00110101;
-	
-	#1000;
-	
-	end
+    logic clk;
+    logic rst_async;
+    logic [N-1:0] init_number;
+    logic [N-1:0] count;
+
+    ContadorRegresivo #(.N(N)) uut (
+        .clk(clk),
+        .rst_async(rst_async),
+        .init_number(init_number),
+        .count(count)
+    );
+
+    // Clock generation
+    always #5 clk = ~clk; // 10 time units clock period
+
+    initial begin
+        // Initialize signals
+        clk = 0;
+			rst_async = 0;
+		  init_number = 8'd6;
+			#10 rst_async = 1;
+			
+        // Let the counter decrement
+        #100;
+
+		  $stop;
+    end
 endmodule
+
