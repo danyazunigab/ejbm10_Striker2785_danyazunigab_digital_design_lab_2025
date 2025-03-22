ca015e229c65fab3c7022930da617dec8bf50d1a
ejbm10 <eduardo.bm10@estudiantec.cr>
2025-03-21 18:07:26 -0600

    configuring alu

--- ALU.sv
+++ ALU.sv
@@ -1,3 +1,35 @@
-
-
+module ALU #(parameter N = 8) (
+	input logic clk,
+	input logic [N-1:0] number,
+	input logic mux1,
+	input logic mux2,
+	input logic op1,
+	input logic op2,
+	input logic op3,
+	input logic eq,
+	output logic [6:0] segs1,
+	output logic [6:0] segs2,
+	output logic [6:0] segs3,
+	output logic [6:0] segs4
+);
 	
+	logic load1, load2;
+	logic [N-1:0] first_number;
+	logic [N-1:0] second_number;
+	
+	assign load1 = 0;
+	assign load2 = 0;
+	
+	logic [] operations;
+	
+	always_ff @(posedge clk) begin
+		if (op1) begin
+			first_number <= number;
+			load1 <= 1;
+		end
+		else if (eq && load1) begin
+			second_number <= number;
+			load2 <= 1;
+		end
+	end
+endmodule
--- tb.sv
+++ tb.sv
@@ -1,18 +1,47 @@
 module tb();
 	
-	parameter N = 6;
-	logic [N-1:0] A, B, Y;
+	parameter N = 8;
+	logic clk, mux1, mux2, op1, op2, op3, eq;
+	logic [N-1:0] number;
+	logic [6:0] segs1, segs2, segs3, segs4;
 	
-	n_bit_and #(.N(N)) dut(
-		.A(A),
-		.B(B),
-		.Y(Y)
+	always #10 clk = ~clk;
+	
+	ALU #(.N(N)) dut(
+		.clk(clk),
+		.number(number),
+		.mux1(mux1),
+		.mux2(mux2),
+		.op1(op1),
+		.op2(op2),
+		.op3(op3),
+		.eq(eq),
+		.segs1(segs1),
+		.segs2(segs2),
+		.segs3(segs3),
+		.segs4(segs4)
 	);
 	
 	initial begin
-		A = 5'b111101;
-		B = 5'b010011;
+		clk = 0;
+		number = 8'b00101101;
+		op1 = 0;
+		eq = 0;
 		
-		#500;
+		
+		#100;
+		
+		op1 = 1;
+		
+		#100;
+		
+		number = 8'b11110000;
+		
+		#100;
+		
+		eq = 1;
+		
+		#100;
+		
 	end
 endmodule
