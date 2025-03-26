178f4115fc051b496ee869c55b10baa1ff4b76c8
ejbm10 <eduardo.bm10@estudiantec.cr>
2025-03-25 20:36:36 -0600

    Alu completely configured

--- ALU.sv
+++ ALU.sv
@@ -6,71 +6,98 @@ module ALU #(parameter N = 4) (
 	input logic mux2,
 	input logic mux3,
 	input logic mux4,
-	input logic load,
+	input logic load1,
+	input logic load2,
 	input logic equal,
 	output logic [6:0] segs1,
 	output logic [6:0] segs2,
+	output logic [6:0] segs3,
 	output logic [3:0] flags
 );
+	logic show;
+	logic [N-1:0] first_number, second_number;
 	
-	logic load1, load2;
-	logic [N-1:0] first_number, second_number, and_res;
-	logic [N:0] sum, res;
-	logic [2*N-1:0] mul, current_number;
+	logic [N:0] add_result;
+	logic [N:0] sub_result;
+	logic [2*N-1:0] mul_result;
+	logic [N-1:0] div_result;
+	logic [N-1:0] mod_result;
+	logic [N-1:0] and_result;
+	logic [N-1:0] or_result;
+	logic [N-1:0] xor_result;
+	logic [N-1:0] shift_right;
+	logic [N-1:0] shift_left;
+	
+	logic [2*N-1:0] current_number;
 	
 	n_bit_adder #(.N(N)) adder (
 		.A(first_number),
 		.B(second_number),
-		.S(sum)
+		.S(add_result)
 	);
 	
 	n_bit_substractor #(.N(N)) substractor (
 		.A(first_number),
 		.B(second_number),
-		.D(res)
+		.D(sub_result)
 	);
 
 	n_bit_multiplier #(.N(N)) multiplier (
 		.A(first_number),
 		.B(second_number),
-		.M(mul)
+		.M(mul_result)
 	);
 	
 	n_bit_and #(.N(N)) andd (
 		.A(first_number),
 		.B(second_number),
-		.Y(and_res)
+		.Y(and_result)
 	);
 	
 	seven_seg_decoder #(.N(N)) displays (
 		.n(current_number),
 		.seven_seg1(segs1),
-		.seven_seg2(segs2)
+		.seven_seg2(segs2),
+		.seven_seg3(segs3)
 	);
 	
 	always_ff @(posedge clk or negedge rst) begin
 		if (!rst) begin
 			first_number <= 0;
 			second_number <= 0;
-			load1 <= 0;
-			load2 <= 0;
+			show <= 0;
 		end
-		else if (!load1) begin
+		else if (!load1)
+			first_number <= number;
+		else if (!load2)
+			second_number <= number;
+		else if (!equal)
+			show <= 1;
+		else if (show) begin
+			case ({mux1, mux2, mux3, mux4})
+				4'b0000:
+					current_number <= add_result;
+				4'b0001:
+					current_number <= sub_result;
+				4'b0010:
+					current_number <= mul_result;
+				4'b0011:
+					current_number <= div_result;
+				4'b0100:
+					current_number <= mod_result;
+				4'b0101:
+					current_number <= and_result;
+				4'b0110:
+					current_number <= or_result;
+				4'b1000:
+					current_number <= xor_result;
+				4'b1001:
+					current_number <= shift_right;
+				4'b1010:
+					current_number <= shift_left;
+			endcase
+		end
+		else
 			current_number <= number;
-			if (!load) begin
-				first_number <= number;
-				load1 <= 1;
-			end
-		end
-		else if (!load2) begin
-			current_number <= number;
-			if (!load) begin
-				second_number <= number;
-				load2 <= 1;
-			end
-		end
-		else if (!equal) begin
-			current_number <= sum;
-		end
 	end
 endmodule
--- seven_seg_decoder.sv
+++ seven_seg_decoder.sv
@@ -1,23 +1,43 @@
 module seven_seg_decoder #(parameter N = 4)(
 	input logic [2*N-1:0] n,
 	output logic [6:0] seven_seg1,
-	output logic [6:0] seven_seg2
+	output logic [6:0] seven_seg2,
+	output logic [6:0] seven_seg3
 );
 	
-	assign seven_seg1[0] = (~n[3] & ~n[2] & ~n[1] & n[0]) | (~n[3] & n[2] & ~n[1] & ~n[0]) | (n[3] & ~n[2] & n[1] & n[0]) | (n[3] & n[2] & ~n[1] & n[0]);
-	assign seven_seg1[1] = (n[2] & n[1] & ~n[0]) | (n[3] & n[1] & n[0]) | (n[3] & n[2] & ~n[0]) | (~n[3] & n[2] & ~n[1] & n[0]);
-	assign seven_seg1[2] = (n[3] & n[2] & ~n[0]) | (n[3] & n[2] & n[1]) | (~n[3] & ~n[2] & n[1] & ~n[0]);
-	assign seven_seg1[3] = (~n[2] & ~n[1] & n[0]) | (n[2] & n[1] & n[0]) | (~n[3] & n[2] & ~n[1] & ~n[0]) | (n[3] & ~n[2] & n[1] & ~n[0]);
-	assign seven_seg1[4] = (~n[3] & n[0]) | (~n[2] & ~n[1] & n[0]) | (~n[3] & n[2] & ~n[1]);
-	assign seven_seg1[5] = (~n[3] & ~n[2] & n[0]) | (~n[3] & ~n[2] & n[1]) | (~n[3] & n[1] & n[0]) | (n[3] & n[2] & ~n[1] & n[0]);
-	assign seven_seg1[6] = (~n[3] & ~n[2] & ~n[1]) | (~n[3] & n[2] & n[1] & n[0]) | (n[3] & n[2] & ~n[1] & ~n[0]);
-	
-	assign seven_seg2[0] = (~n[7] & ~n[6] & ~n[5] & n[4]) | (~n[7] & n[6] & ~n[5] & ~n[4]) | (n[7] & ~n[6] & n[5] & n[4]) | (n[7] & n[6] & ~n[5] & n[4]);
-	assign seven_seg2[1] = (n[6] & n[5] & ~n[4]) | (n[7] & n[5] & n[4]) | (n[7] & n[6] & ~n[4]) | (~n[7] & n[6] & ~n[5] & n[4]);
-	assign seven_seg2[2] = (n[7] & n[6] & ~n[4]) | (n[7] & n[6] & n[5]) | (~n[7] & ~n[6] & n[5] & ~n[4]);
-	assign seven_seg2[3] = (~n[6] & ~n[5] & n[4]) | (n[6] & n[5] & n[4]) | (~n[7] & n[6] & ~n[5] & ~n[4]) | (n[7] & ~n[6] & n[5] & ~n[4]);
-	assign seven_seg2[4] = (~n[7] & n[4]) | (~n[6] & ~n[5] & n[4]) | (~n[7] & n[6] & ~n[5]);
-	assign seven_seg2[5] = (~n[7] & ~n[6] & n[4]) | (~n[7] & ~n[6] & n[5]) | (~n[7] & n[5] & n[4]) | (n[7] & n[6] & ~n[5] & n[4]);
-	assign seven_seg2[6] = (~n[7] & ~n[6] & ~n[5]) | (~n[7] & n[6] & n[5] & n[4]) | (n[7] & n[6] & ~n[5] & ~n[4]);
-	
+	logic [3:0] digit1, digit2, digit3;
+    
+    always_comb begin
+        int temp;
+        temp = n;
+        
+        digit1 = temp % 10;
+        temp = temp / 10;
+        
+        digit2 = temp % 10;
+        temp = temp / 10;
+        
+        digit3 = temp % 10;
+    end
+    
+    function logic [6:0] bcd_to_7seg(input logic [3:0] bcd);
+        case (bcd)
+            4'd0: bcd_to_7seg = 7'b1000000;
+            4'd1: bcd_to_7seg = 7'b1111001;
+            4'd2: bcd_to_7seg = 7'b0100100;
+            4'd3: bcd_to_7seg = 7'b0110000;
+            4'd4: bcd_to_7seg = 7'b0011001;
+            4'd5: bcd_to_7seg = 7'b0010010;
+            4'd6: bcd_to_7seg = 7'b0000010;
+            4'd7: bcd_to_7seg = 7'b1111000;
+            4'd8: bcd_to_7seg = 7'b0000000;
+            4'd9: bcd_to_7seg = 7'b0010000;
+            default: bcd_to_7seg = 7'b1111111;
+        endcase
+    endfunction
+    
+    assign seven_seg1 = bcd_to_7seg(digit1);
+    assign seven_seg2 = bcd_to_7seg(digit2);
+    assign seven_seg3 = bcd_to_7seg(digit3);
+    
 endmodule
